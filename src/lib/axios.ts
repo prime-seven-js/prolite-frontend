edb98f618d733c6a8bd887fc89f5ac817b54dc0f
Đỗ Hiếu <98524125+leint0708@users.noreply.github.com>
2026-03-19 23:18:29 +0700

    second commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8787",
+  baseURL: "https://api.prolite.gay",
 });
 
 api.interceptors.request.use((config) => {
